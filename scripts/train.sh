b35c0e6b6bb87b3d40e1f1e38644ec873e6f065e
janinelaura.hindermann <janinelaura.hindermann@uzh.ch>
2024-04-19 16:54:42 +0200

    modified main.py for logging ppl

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,21 +6,27 @@ base=$(realpath $scripts/..)
 models=$base/models
 data=$base/data
 tools=$base/tools
+logs=$base/logs
 
 mkdir -p $models
+mkdir -p $logs
 
 num_threads=4
 device=""
 
 SECONDS=0
 
-(cd $tools/pytorch-examples/word_language_model &&
-    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python main.py --data $data/familyguy \
-        --epochs 100 \
-        --log-interval 100 \
-        --emsize 200 --nhid 200 --dropout 0.3 --tied \
-        --save $models/model.pt
+(
+  cd $tools/pytorch-examples/word_language_model &&
+    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python3 main.py \
+      --data $data/familyguy \
+      --epochs 2 \
+      --log-interval 100 \
+      --emsize 200 --nhid 200 --dropout 0.3 --tied \
+      --save $models/model.pt \
+      --mps \
+      --save-perplexities \
+      --log-file $logs/perplexities_0.3.txt \
 )
-
 echo "time taken:"
 echo "$SECONDS seconds"
